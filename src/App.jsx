44de618908e5a360dcec82a8a5ef2004fe6db222
takanasi <1264791291@qq.com>
2022-08-15 23:23:44 +0800

    v5

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import 'antd/dist/antd.less';
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import {Button, message} from 'antd';
+
+import Login from './pages/login/login.jsx';
+import Admin from './pages/admin/admin.jsx';
+
 class App extends Component {
     state = {  } 
+    handleClick = () => {
+        message.success('this is a message of success');
+    }
     render() { 
         return (
-            <h1>1takanasi</h1>
-            
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/login' element={<Login></Login>}></Route>
+                    <Route path='/admin' element={<Admin></Admin>}></Route>
+                </Routes>
+            </BrowserRouter>
         );
     }
 }
---
+++ src/pages/admin/admin.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class Admin extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <h1>admin</h1>
+        );
+    }
+}
+ 
+export default Admin;
