35152c1f1df486e181046fe34d200ca0ff293733
takanasi <1264791291@qq.com>
2022-09-04 14:57:13 +0800

    v7-login

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,14 +1,48 @@
 import React, { Component } from 'react';
-import {  Button, Form, Input } from "antd";
+import {  Button, Form, Input,message } from "antd";
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
 
 import './login.less';
 import logo from './images/logo.png'
 
 class Login extends Component {
-    state = {  } 
+    state = {  }
+    
+    hasDight = (str)=>{
+        for(let x of str){
+            if(x >= '0' && x <= '9'){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    checkPassword = (str) =>{
+        let size = str.length;
+        if(size <= 6){
+            return false;
+        }
+        return true;
+    }
+    
     handleSubmit = (values) =>{
-        console.log(values);
+        let {user:username,password} = values;
+        
+        if((username === null || username === '' || username === undefined) || (password === null || password === '' || password === undefined)){
+            message.error('sorry, username or password are not allowed to be empty')
+        }
+        else if(!this.hasDight(username)){
+            message.error('username can not have any digit');
+        }
+        else {
+            if(!this.checkPassword(password)){
+                message.warning('waring, your password is too short, it is danger!');
+            }
+            setTimeout(()=>{
+                message.success('login successfully');
+            },3200)
+        }
+        
     };
 
     render() { 
@@ -21,10 +55,10 @@ class Login extends Component {
                 <section className='login-content'>
                     <h2>用户登录</h2>
                     <Form name="normal_login"  className="login-form" onFinish={this.handleSubmit}>
-                        <Form.Item>
+                        <Form.Item name={'user'}>
                             <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="用户名" />
                         </Form.Item>
-                        <Form.Item>
+                        <Form.Item name={'password'}>
                             <Input
                                 prefix={<LockOutlined className="site-form-item-icon" />}
                                 type="password"
